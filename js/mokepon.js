7596b7cfe58a453b8e356c2435acb95969dfa8b8
SirFrank0 <frankseb04@gmail.com>
2022-08-19 21:20:04 -0500

    Se agrega primer evento para un ID de HTML en JS

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,1 +1,9 @@
-alert("Hola mundo")
+//En JS no se puede utilizar "-" para separar nombres
+
+function seleccionarMascotaJugador(){                   
+    alert("Selccionaste tu mascota")
+}
+
+let botonMascotaJugador = document.getElementById('boton-mascota')                  //Se crea variable para invocar al "id=boton-mascota" de HTML
+
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)         //Se le agrega un evento a la variable "botonMascotaJugador" para que invoque la funcion "seleccionarMascotaJugador" al hacer click
