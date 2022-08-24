a76e864afb55d4a016c3f6a84620dd9767fab843
SirFrank0 <frankseb04@gmail.com>
2022-08-23 23:42:22 -0500

    Se agrega evento para cargar el JS

--- js/mokepon.js
+++ js/mokepon.js
@@ -4,6 +4,17 @@ function seleccionarMascotaJugador(){
     alert("Selccionaste tu mascota")
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')                  //Se crea variable para invocar al "id=boton-mascota" de HTML
 
-botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)         //Se le agrega un evento a la variable "botonMascotaJugador" para que invoque la funcion "seleccionarMascotaJugador" al hacer click
+//Se crea la funcion para que la escuhe el evento 'load'
+
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')                  //Se crea variable para invocar al "id=boton-mascota" de HTML
+
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)                 //Se le agrega un evento a la variable "botonMascotaJugador" para que invoque la funcion "seleccionarMascotaJugador" al hacer click
+
+}
+
+        
+//Se crea un evento para que el codigo JS arranque apenas se cargue la pagina 
+
+window.addEventListener('load', iniciarJuego)
