90174b678557e0bff807b8351d55bd539f68b6ec
SirFrank0 <frankseb04@gmail.com>
2022-08-24 00:10:38 -0500

    Se crean if para notificar mascota selecccionada

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,9 +1,3 @@
-//En JS no se puede utilizar "-" para separar nombres
-
-function seleccionarMascotaJugador(){                   
-    alert("Selccionaste tu mascota")
-}
-
 
 //Se crea la funcion para que la escuhe el evento 'load'
 
@@ -12,6 +6,26 @@ function iniciarJuego(){
 
 botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)                 //Se le agrega un evento a la variable "botonMascotaJugador" para que invoque la funcion "seleccionarMascotaJugador" al hacer click
 
+}
+
+//Se crea una funcion para guardar el evento de mostrar mascota segun el input seleccionado
+
+function seleccionarMascotaJugador(){            
+    
+    //Se crean variables para llamarlas al crear condiciones
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    //Se crea la condicion para notificar al usuario la mascota segun el input seleccionado
+
+    if (inputHipodoge.checked){                 //checked siginifca si el input, en este caso 'radio' es falso o verdadero (si esta seleccionado o no)
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked){
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked){
+        alert('Seleccionaste a Ratigueya')
+    }
 }
 
         
